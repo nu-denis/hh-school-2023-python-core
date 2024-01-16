df60c9384f747a7fca9c155be758925e01b74646
Ruslan <grr_95@mail.ru>
2024-01-16 15:22:03 +0300

    add solution

--- hw/main.py
+++ hw/main.py
@@ -1,6 +1,7 @@
 from wine import Wine
 from beer import Beer
 from market import Market
+from random import randint, shuffle
 
 """
 TODO: Доработать заготовки классов вина (Wine), пива (Beer) и магазина (Market) таким образом, чтобы через класс Market можно было:
@@ -10,3 +11,28 @@ TODO: Доработать заготовки классов вина (Wine), п
     * получить список напитков (вина и пива) в указанном диапазоне даты производства
     * (*) написать свой декоратор, который бы логировал начало выполнения метода и выводил время выполнения
 """
+
+vkusvill = Market(
+    wines=[
+        Wine("vine 1", 2024),
+        Wine("vine 2", 2023),
+        Wine("vine 4", 2022),
+        Wine("vine 7", 2021),
+        Wine("vine 3", 2020),
+        Wine("vine 6", 2020),
+    ],
+    beers=[
+        Beer("beer 1", 2022),
+        Beer("beer 3", 2022),
+        Beer("beer 2", 2021),
+        Beer(None, 2021),
+        Beer("beer 4", None),
+    ]
+)
+
+vkusvill.has_drink_with_title('vine 1')
+vkusvill.has_drink_with_title('vine 11')
+vkusvill.get_drinks_by_production_date(None,2023)
+vkusvill.get_drinks_by_production_date(2021,None)
+vkusvill.get_drinks_by_production_date(None,None)
+vkusvill.get_drinks_sorted_by_title()
--- hw/market.py
+++ hw/market.py
@@ -1,7 +1,21 @@
+from datetime import datetime
+
+def function_info_decorator(func):
+    def wrapper(*args, **kwargs):
+        print('\nСтарт функции', func.__qualname__)
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        print('Финиш. Затраченное время: ', datetime.now() - start)
+        print('Результат выполнения:\n', result)
+        return result
+    return wrapper
+
 class Market:
     def __init__(self, wines: list = None, beers: list = None) -> None:
-        pass
+        self.wines = { drink.title: drink for drink in wines }
+        self.beers = { drink.title: drink for drink in beers }
 
+    @function_info_decorator
     def has_drink_with_title(self, title=None) -> bool:
         """
         Проверяет наличие напитка в магазине за О(1)
@@ -9,20 +23,49 @@ class Market:
         :param title:
         :return: True|False
         """
-        pass
+        return title in self.wines or title in self.beers
 
+    @function_info_decorator
     def get_drinks_sorted_by_title(self) -> list:
         """
         Метод получения списка напитков (вина и пива) отсортированных по title
 
         :return: list
         """
-        pass
+        return sorted([*self.beers, *self.wines], key=lambda x: (x is None, x))
 
+    @function_info_decorator
     def get_drinks_by_production_date(self, from_date=None, to_date=None) -> list:
         """
         Метод получения списка напитков в указанном диапазоне дат: с from_date по to_date
 
         :return: list
         """
-        pass
+        drinks = [*self.beers.values(), *self.wines.values()]
+        filtered_drinks =  list(filter(lambda drink: self.__date_in_range(drink, from_date, to_date), drinks))
+        result = list()
+
+        for drink in filtered_drinks:
+            result.append(drink.title)
+
+        return result
+
+    def __date_in_range(self, drink, from_date=None, to_date=None) -> bool:
+        """
+        Метод проверяющий входит ли production_date в from_date : to_date
+
+        :return: bool
+        """
+        if drink.production_date is None:
+            return False
+
+        if not from_date and not to_date:
+            return True
+        
+        if not from_date:
+            return drink.production_date <= to_date
+        
+        if not to_date:
+            return drink.production_date >= from_date
+
+        return from_date <= drink.production_date <= to_date
